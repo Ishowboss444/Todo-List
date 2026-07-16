652124389c8640232f629cedda4ca1760165676b
Ishowboss444 <abolboss1388@gmail.com>
2026-07-17 00:24:46 +0330

    feat(sidebar)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import VSidebar from './components/VSidebar.vue';
 </script>
 
 <template>
+    <aside class="aside">
+     <VSidebar/>
+    </aside>
+    <div class="main-section"></div>
 </template>
 
 <style scoped>
-
+    
 </style>
---
+++ src/components/VSidebar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="padded-aside">
+        <div class="card-close">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="card-close">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="card-close">
+            <i class="fa-solid fa-gear"></i>
+        </div>
+    </div>
+</template>
