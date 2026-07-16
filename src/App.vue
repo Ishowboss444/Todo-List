eccae2681b0eac6394e6935aaba752338b9234e9
Ishowboss444 <abolboss1388@gmail.com>
2026-07-17 01:27:31 +0330

    fix(searchBox and Id bug)
    now If you click on Add button the search box would be empty and the Id of the object Got fixed As well

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import { computed, reactive, ref } from 'vue';
+import { computed, reactive, ref,watch } from 'vue';
 import VSidebar from './components/VSidebar.vue';
 import VCard from './components/VCard.vue';
-const title = ref(null)
 const isDark = ref(true)
 const todosArray = ref([])
 const id = ref(0)
-const todosObject ={
-    title : title.value,
+const todosObject = ref({
+    title : "",
     completed : false,
     Edit :false,
     id:id.value,
-}
+})
 const isEmpty = ref(true)
 function fun(){
-    if(title.value === null){
+    if(todosObject.value.title === ""){
         alert("complete the input")
         return
     }else{
-        todosArray.value.push(
-            
-        )
+        todosObject.value.id = id.value
+        todosArray.value.push({...todosObject.value})
+        todosObject.value.title = ''
         console.log(todosArray.value);
-        id.value++
+        id.value += 1
     }
-    
 }
 function theme(){
     isDark.value = !isDark.value
@@ -43,10 +41,10 @@ function theme(){
         <h1>My Tasks</h1>
       </div>
       <div class="search-section">
-        <input type="text" class="search" placeholder="Type your task here..." v-model="title">
+        <input type="text" class="search" placeholder="Type your task here..." v-model="todosObject.title">
         <button @click="fun()">+ Add</button>
         </div>
-        <div v-if="isEmpty" class="empty-card">empty</div>
+        <div v-if="isEmpty" class="empty-card" @click="test">empty</div>
         <div v-else class="card">
             <VCard/>
         </div>
