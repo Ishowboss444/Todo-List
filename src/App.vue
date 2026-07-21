5fadb44b719cc1d22e31b43cb2b0dc04ee727ebd
Ishowboss444 <abolboss1388@gmail.com>
2026-07-21 15:07:22 +0330

    feat(edit)
    the ability on editing has been successfully coded ,however this might be changed if being an disable input reduce the performance.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,91 @@
 <script setup>
-import { computed, reactive, ref,watch } from 'vue';
+import {computed,ref} from 'vue';
 import VSidebar from './components/VSidebar.vue';
 import VCard from './components/VCard.vue';
+
 const isDark = ref(true)
 const todosArray = ref([])
 const id = ref(0)
 const todosObject = ref({
-    title : "",
-    completed : false,
-    Edit :false,
-    id:id.value,
+  title: "",
+  completed: false,
+  Edit: false,
+  id: id.value,
 })
-const isEmpty = computed(()=>{
-    if(todosArray.value.length > 0){
-        return false
-    }else{
-        return true
-    }
+const isEmpty = computed(() => {
+  if (todosArray.value.length > 0) {
+    return false
+  } else {
+    return true
+  }
 })
-function adder(){
-    if(todosObject.value.title === ""){
-        alert("complete the input")
-        return
-    }else{
-        todosObject.value.id = id.value
-        todosArray.value.push({...todosObject.value})
-        todosObject.value.title = ''
-        id.value += 1
-    }
+
+function adder() {
+  if (todosObject.value.title === "") {
+    alert("complete the input")
+  } else {
+    todosObject.value.id = id.value
+    todosArray.value.push({...todosObject.value})
+    todosObject.value.title = ''
+    id.value += 1
+  }
 }
-function theme(){
-    isDark.value = !isDark.value
+
+function theme() {
+  isDark.value = !isDark.value
 }
-function test(index){
-    console.log(index.id);
-    todosArray.value.splice(index.index,1)
+
+function deleted(index) {
+  todosArray.value.splice(index.index, 1)
+}
+function edited(index){
+  console.log(todosArray.value[index.index].title)
 }
 </script>
 
 <template>
-    <aside class="aside"
-    >
-     <VSidebar/>
-    </aside>
+  <aside class="aside"
+  >
+    <VSidebar/>
+  </aside>
 
-    <div class="main-section">
-      <div class="header">
+  <div class="main-section">
+    <div class="header">
         <span class="darkness"
-         @click="theme"
-         ><i :class="isDark ?
+              @click="theme"
+        ><i :class="isDark ?
          'fa-regular fa-sun' :
          'fa-regular fa-moon'"
-          ></i>
+        ></i>
         </span>
-      </div>
-      <div class="central-holder">
+    </div>
+    <div class="central-holder">
       <div class="logo">
         <h1>My Tasks</h1>
       </div>
       <div class="search-section">
         <input type="text"
-        class="search" 
-        placeholder="Type your task here..." 
-        v-model="todosObject.title"
+               class="search"
+               placeholder="Type your task here..."
+               v-model="todosObject.title"
         >
-        
+
         <button @click="adder()">+ Add</button>
       </div>
-      
-      <div v-if="isEmpty" 
-      class="empty-card" 
-      @click="test"
-      >empty</div>
-      
+
+      <div v-if="isEmpty"
+           class="empty-card"
+      >empty
+      </div>
+
       <div v-else class="card">
-        <VCard 
-        :title="todosArray"
-        @delete="test"
+        <VCard
+            :title="todosArray"
+            @delete="deleted"
+            @edited="edited"
         />
       </div>
 
-      </div>
     </div>
+  </div>
 </template>
--- src/components/VCard.vue
+++ src/components/VCard.vue
@@ -1,31 +1,44 @@
 <script setup>
-    const props = defineProps({
-        title: Object
-    })
-    const emit = defineEmits(['delete'])
-    function indexSender(index){
-        emit('delete', {index:index,id:1})
-    }
+import {ref, watch} from "vue";
+
+const onEdit = ref(false)
+watch(onEdit , ()=>{
+  console.log(onEdit.value)
+})
+const props = defineProps({
+  title: Object
+})
+const emits = defineEmits(['delete' , 'edited'])
+function deleteSender(index) {
+  emits('delete', {index: index})
+}
+function editSender(index){
+  emits('edited', {index: index})
+}
 </script>
 <template>
-    <div  class="masterCard" v-for="(item , index) in props.title">
-        <div class="cards">
-            <div class="order-of-cards">
-                <input type="checkbox">
-            </div>
-            <div class="order-of-cards">
-                <h3 :class="props.title[index].completed?
-                'lined' : ''">{{ props.title[index].title }}</h3>
-            </div>
-            <div class="icons order-of-cards">
-                <i class="fa-regular fa-pen-to-square"></i>
-            </div>
-            <div class="icons order-of-cards">
-                <i 
-                @click="indexSender(index)"
-                class="fa-regular fa-trash-can"
-            ></i>
-            </div>
-        </div>
+  <div class="masterCard" v-for="(item , index) in props.title">
+    <div class="cards">
+      <div class="order-of-cards">
+        <input type="checkbox">
+      </div>
+      <div class="order-of-cards">
+        <input class="EditorInput" type="text" v-model="item.title"
+               :id="onEdit ? 'disabled' : 'enabled'"
+               :disabled="onEdit">
+      </div>
+      <div class="icons order-of-cards">
+        <i
+            @click="editSender(index); onEdit = !onEdit"
+            class="fa-regular fa-pen-to-square"
+        ></i>
+      </div>
+      <div class="icons order-of-cards">
+        <i
+            @click="deleteSender(index)"
+            class="fa-regular fa-trash-can"
+        ></i>
+      </div>
     </div>
+  </div>
 </template>
