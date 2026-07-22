96475d9b2e885dd9875e14d7be1c36b4326b2464
Ishowboss444 <abolboss1388@gmail.com>
2026-07-22 13:25:19 +0330

    fix(edit , style)
    edit problem has fixed successfully and some styles has already coded(dark and light mode)

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,101 @@
 <script setup>
-import {computed,ref} from 'vue';
-import VSidebar from './components/VSidebar.vue';
-import VCard from './components/VCard.vue';
+    import { computed, ref } from 'vue';
+    import VSidebar from './components/VSidebar.vue';
+    import VCard from './components/VCard.vue';
+    import VEmptyImg from '@/components/VEmptyImg.vue';
 
-const isDark = ref(true)
-const todosArray = ref([])
-const id = ref(0)
-const todosObject = ref({
-  title: "",
-  completed: false,
-  edit: false,
-  id: id.value,
-})
-const isEmpty = computed(() => {
-  if (todosArray.value.length > 0) {
-    return false
-  } else {
-    return true
-  }
-})
+    const isDark = ref(true);
+    const todosArray = ref([]);
+    const id = ref(0);
+    const todosObject = ref({
+        title: '',
+        completed: false,
+        edit: false,
+        id: id.value
+    });
+    const isEmpty = computed(() => {
+        if (todosArray.value.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    });
 
-function adder() {
-  if (todosObject.value.title === "") {
-    alert("complete the input")
-  } else {
-    todosObject.value.id = id.value
-    todosArray.value.push({...todosObject.value})
-    todosObject.value.title = ''
-    id.value += 1
-  }
-}
+    function adder() {
+        if (todosObject.value.title === '') {
+            alert('complete the input');
+        } else {
+            todosObject.value.id = id.value;
+            todosArray.value.push({ ...todosObject.value });
+            todosObject.value.title = '';
+            id.value += 1;
+        }
+    }
 
-function theme() {
-  isDark.value = !isDark.value
-}
+    function theme() {
+        isDark.value = !isDark.value;
+    }
 
-function deleted(index) {
-  todosArray.value.splice(index.index, 1)
-}
-function edited(index){
-  console.log(todosArray.value[index.index].title)
-}
+    function deleted(index) {
+        todosArray.value.splice(index.index, 1);
+    }
+
+    function edited(index) {
+        console.log(todosArray.value[index.index].title);
+    }
 </script>
 
 <template>
-  <aside class="aside"
-  >
-    <VSidebar/>
-  </aside>
+    <aside
+        class="aside"
+        :id="!isDark ? 'dark-aside' : 'light-aside'"
+    >
+        <VSidebar/>
+    </aside>
 
-  <div class="main-section">
-    <div class="header">
+    <div
+        class="main-section"
+        :id="!isDark ? 'dark' : 'light'"
+    >
+        <div class="header">
         <span class="darkness"
               @click="theme"
-        ><i :class="isDark ?
-         'fa-regular fa-sun' :
-         'fa-regular fa-moon'"
+        ><i :class="!isDark ?
+            'fa-regular fa-sun isDark' :
+            'fa-regular fa-moon'"
         ></i>
         </span>
+        </div>
+        <div class="central-holder">
+            <div class="logo">
+                <h1>My Tasks</h1>
+            </div>
+            <div class="search-section">
+                <input type="text"
+                       class="search"
+                       placeholder="Type your task here..."
+                       v-model="todosObject.title"
+                >
+                <button class="add-button" @click="adder()">+ Add</button>
+            </div>
+
+            <div v-if="isEmpty"
+                 class="empty-card"
+                 id="special-width-empty"
+            >
+                <VEmptyImg/>
+            </div>
+
+            <div v-else class="card">
+                <VCard
+                    :title="todosArray"
+                    @delete="deleted"
+                    @edited="edited"
+                />
+            </div>
+            <footer>
+                <p>&copy;&nbsp;2026</p>
+            </footer>
+        </div>
     </div>
-    <div class="central-holder">
-      <div class="logo">
-        <h1>My Tasks</h1>
-      </div>
-      <div class="search-section">
-        <input type="text"
-               class="search"
-               placeholder="Type your task here..."
-               v-model="todosObject.title"
-        >
-
-        <button @click="adder()">+ Add</button>
-      </div>
-
-      <div v-if="isEmpty"
-           class="empty-card"
-      >empty
-      </div>
-
-      <div v-else class="card">
-        <VCard
-            :title="todosArray"
-            @delete="deleted"
-            @edited="edited"
-        />
-      </div>
-
-    </div>
-  </div>
 </template>
--- src/components/VCard.vue
+++ src/components/VCard.vue
@@ -1,37 +1,39 @@
 <script setup>
-const props = defineProps({
-  title: Object
-})
-const emits = defineEmits(['delete' , 'edited'])
-function deleteSender(index) {
-  emits('delete', {index: index})
-}
-function editSender(index){
-  emits('edited', {index: index})
-}
+    const props = defineProps({
+        title: Object
+    });
+    const emits = defineEmits(['delete', 'edited']);
+
+    function deleteSender(index) {
+        emits('delete', { index: index });
+    }
+
+    function editSender(index) {
+        emits('edited', { index: index });
+    }
 </script>
 <template>
-  <div class="masterCard" v-for="(item , index) in props.title">
-    <div class="cards">
-      <div class="order-of-cards">
-        <input type="checkbox">
-      </div>
-      <div class="order-of-cards">
-        <h3 v-if="!item.edit">{{item.title}}</h3>
-        <input v-else class="EditorInput" type="text" v-model="item.title">
-      </div>
-      <div class="icons order-of-cards">
-        <i
-            @click="editSender(index); item.edit = !item.edit ;console.log(item.edit)"
-            class="fa-regular fa-pen-to-square"
-        ></i>
-      </div>
-      <div class="icons order-of-cards">
-        <i
-            @click="deleteSender(index)"
-            class="fa-regular fa-trash-can"
-        ></i>
-      </div>
+    <div class="masterCard" v-for="(item , index) in props.title">
+        <div class="cards">
+            <div class="order-of-cards">
+                <input type="checkbox" class="checkbox">
+            </div>
+            <div class="order-of-cards">
+                <h3 class="todo-title" v-if="!item.edit">{{ item.title }}</h3>
+                <input v-else class="EditorInput" type="text" v-model="item.title">
+            </div>
+            <div class="icons order-of-cards">
+                <i
+                    @click="editSender(index); item.edit = !item.edit ;console.log(item.edit)"
+                    class="fa-regular fa-pen-to-square"
+                ></i>
+            </div>
+            <div class="icons order-of-cards">
+                <i
+                    @click="deleteSender(index)"
+                    class="fa-regular fa-trash-can"
+                ></i>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
