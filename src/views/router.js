eea9cb15c1309673790fc02b8f47421d0a9be4e0
Ishowboss444 <abolboss1388@gmail.com>
2026-08-15 13:12:01 +0330

    refactor(lazy loading)

--- src/views/router.js
+++ src/views/router.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import MyTasksView from './MyTasksView.vue';
-import SettingView from '@/views/SettingView.vue';
-import {createPinia} from 'pinia';
-
 
 export const router = createRouter({
     history:createWebHistory(),
     routes: [
-        {path: '/myTasks' ,name:'MyTasksView',component: MyTasksView,},
-        {path: '/SettingView',name: 'SettingView',component: SettingView,},
+        {
+            path: '/myTasks',
+            name:'MyTasksView',
+            component: ()=> import('/MyTasksView.vue'),
+        },
+        {
+            path: '/SettingView',
+            name: 'SettingView',
+            component: ()=> import('/SettingView.vue'),
+        },
     ],
 })
